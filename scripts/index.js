686eb09f704f43af47b676a7f5e663d22c5316bc
Nadezhda Latysheva <nana7305@mail.ru>
2022-10-02 17:35:10 +0300

    fix: Исправить модальное окно

--- scripts/index.js
+++ scripts/index.js
@@ -2,25 +2,30 @@ let editBtn = document.querySelector('.profile__edit-btn');
 let popup = document.querySelector('.popup');
 let closePopupBtn = popup.querySelector('.popup__close-btn');
 let popupForm = popup.querySelector('.popup__form');
-let popupFormNameField = popupForm.querySelector('input[name="name"]');
-let popupFormJobField = popupForm.querySelector('input[name="job"]');
+let popupFormNameField = popupForm.querySelector('.popup__form-item_el_name');
+let popupFormAboutField = popupForm.querySelector('.popup__form-item_el_about');
 let userName = document.querySelector('.profile__name');
-let userJob = document.querySelector('.profile__about');
+let userAbout = document.querySelector('.profile__about');
 
-popupFormNameField.value = userName.textContent;
-popupFormJobField.value = userJob.textContent;
-
-editBtn.addEventListener('click', () => {
+function openPopup() {
   popup.classList.add('popup_opened');
-});
+  popupFormNameField.value = userName.textContent;
+  popupFormAboutField.value = userAbout.textContent;
+}
 
-closePopupBtn.addEventListener('click', () => {
+function closePopup() {
   popup.classList.remove('popup_opened');
-})
+}
 
-popupForm.addEventListener('submit', (evt) => {
+editBtn.addEventListener('click', openPopup);
+
+closePopupBtn.addEventListener('click', closePopup);
+
+function editProfileInfo(evt) {
   evt.preventDefault();
   userName.textContent = popupFormNameField.value;
-  userJob.textContent = popupFormJobField.value;
-  popup.classList.remove('popup_opened');
-});
+  userAbout.textContent = popupFormAboutField.value;
+  closePopup();
+}
+
+popupForm.addEventListener('submit', editProfileInfo);
